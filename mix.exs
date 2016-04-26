ae976f3de6f13b62b00ded8ac8a6b4f946b7c7a5
Ashley Williams <hi@ashleyw.co.uk>
2016-04-26 21:45:19 +0100

    Add .travis.yml

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule NaiveBayes.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :naive_bayes,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     package: package,
-     description: description,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :naive_bayes,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      package: package,
+      description: description,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+   ]
   end
 
   def application do
