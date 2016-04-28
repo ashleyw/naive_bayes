2438dafc425ed7afe39d40a557367d8a4c054b77
Ashley Williams <hi@ashleyw.co.uk>
2016-04-28 21:56:55 +0100

    Remove redundant line

--- lib/naive_bayes/vocab.ex
+++ lib/naive_bayes/vocab.ex
@@ -2,8 +2,7 @@ defmodule Vocab do
   defstruct tokens: %{}
 
   def seen_token(vocab, token) do
-    vocab = put_in(vocab.tokens[token], true)
-    vocab
+    put_in(vocab.tokens[token], true)
   end
 
   def remove_token(vocab, token) do
